842511a3f0c70163cc5cf54afaf446632adc14a2
Howey Guo <2411360636@qq.com>
2025-04-14 00:04:47 -0700

    chore: remove .history directory and update gitignore

--- .history/src/db/rdbms_connector_20250413181008.py
+++
@@ -1,7 +0,0 @@
-# Handles MySQL/PostgreSQL database connection
-import pymysql
-
-
-def connect_to_rdbms():
-    """Establishes a connection to an RDBMS."""
-    return pymysql.connect(host="localhost", user="root", password="", database="dsci551")
